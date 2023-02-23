4d86060e10606c19754a50bad268d1ea9efabbea
shrikant Lohar <shrikantlohar91@gmail.com>
2023-02-23 11:35:38 +0530

    "Deleted TC 3 "

--- AndroidAppTesting/AndroidTestRunner/runner.py
+++ AndroidAppTesting/AndroidTestRunner/runner.py
@@ -3,8 +3,12 @@ from datetime import datetime
 
 from robot import run
 
-todayDate = datetime.now()
-print(todayDate)
-reportDirectory = os.path.join('../AndroidTestResults/', datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
-os.makedirs(reportDirectory)
-run('../AndroidTestSuite', outputdir=reportDirectory)
+
+def runTest():
+    reportDirectory = os.path.join('../AndroidTestResults/', datetime.now().strftime('%d-%m-%y_%H-%M-%S'))
+    os.makedirs(reportDirectory)
+    run('../AndroidTestSuite', outputdir=reportDirectory)
+
+
+class runner:
+    runTest()

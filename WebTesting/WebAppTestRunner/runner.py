017730f461b8299319d8901f00ab6c125bb013ea
shrikant Lohar <shrikantlohar91@gmail.com>
2023-02-23 11:48:49 +0530

    Updating RF

---
+++ WebTesting/WebAppTestRunner/runner.py
@@ -0,0 +1,19 @@
+import os.path
+from datetime import datetime
+
+from robot import run
+
+
+def runTest():
+    reportDirectory = os.path.join('../AndroidTestResults/', datetime.now().strftime('%d-%m-%y_%H-%M-%S'))
+    os.makedirs(reportDirectory)
+    run('../AndroidTestSuite', outputdir=reportDirectory)
+
+
+def sendEmail():
+    pass
+
+
+class runner:
+    if __name__ == "__main__":
+        runTest()
